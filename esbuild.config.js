1f6f6e47d598f27dfd584e943eb9e4b81cd64763
yuta4j1 <kascado.ys10@gmail.com>
2021-05-02 15:52:49 +0900

    更新

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,7 +2,6 @@ require('esbuild')
   .build({
     entryPoints: ['index.ts'],
     bundle: true,
-    minify: process.env.NODE_ENV === 'prod',
     platform: 'node',
     outfile: 'dist/app.js',
   })
